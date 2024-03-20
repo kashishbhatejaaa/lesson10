3b8b3d0f4b9922aaf8120f445217b348daa530c7
kashishbhatejaaa <144736436+kashishbhatejaaa@users.noreply.github.com>
2024-03-20 12:38:48 -0400

    lab completed again

--- js/json.js
+++ js/json.js
@@ -7,7 +7,7 @@ async function populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
     const requestURL = "https://nvvme.github.io/comp1073Lesson10/js/i-scream.json"; 
-    // URL taken from group 11
+    // URL taken from group 10
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
